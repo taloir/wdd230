cce000aedf1fd8bf33e822ddf529b592629ac50f
taloir <taloir.twl@gmail.com>
2024-03-14 20:12:42 -0600

    applied css to weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.63}&units=imperial&appid={d629a871051a7e971a6a9bc52d109f8a}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=imperial&appid=d629a871051a7e971a6a9bc52d109f8a';
  
 
 async function apiFetch() {
@@ -10,8 +10,8 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
-        // displayResults(data);
+        //console.log(data);
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -20,4 +20,13 @@ async function apiFetch() {
     }
   }
   
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
   apiFetch();
